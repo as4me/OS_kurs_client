825cda49023c816511d1cf75243b094bdc366e37
Andrew Sergeev <wantedmins@gmail.com>
2022-12-13 00:03:37 +0300

    Initial commit

---
+++ app/src/main/java/ru/mtuci/os_kurs_client/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.mtuci.os_kurs_client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
